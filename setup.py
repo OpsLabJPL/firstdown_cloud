17392c0b7d28d8c79cf5e546fcbc9eb937f795f8
Mark Powell <Mark.Powell@jpl.nasa.gov>
2018-06-01 09:28:15 -0700

    add license and URL

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='firstdown_cloud',
     version='0.0.2',
-    url='https://github.jpl.nasa.gov/mpowell/FirstDownCloud',
-    license='',
+    url='https://github.com/OpsLabJPL/firstdown_cloud',
+    license='Apache 2.0',
     author='Mark Powell',
     author_email='Mark.W.Powell@jpl.nasa.gov',
     description='Python library for securely publishing JPL ops summary data to Govcloud.',
